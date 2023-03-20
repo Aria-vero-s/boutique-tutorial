69012347d25ee4ddede3f3f54ec95350a287bdd8
Aria-vero-s <ariane.saulnier@gmail.com>
2023-03-20 19:59:13 +0000

    Linked checkbox with javascript

--- static/js/script.js
+++ static/js/script.js
@@ -46,22 +46,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("service") === "branding2") {
+            if (this.getAttribute("package") === "branding2") {
                 bronze.style.display = "inline";
                 silver.style.display = "inline";
                 gold.style.display = "inline";
             }
-            if (this.getAttribute("service") === "website2") {
+            if (this.getAttribute("package") === "website2") {
                 bronze.style.display = "inline";
                 silver.style.display = "inline";
                 gold.style.display = "inline";
             }
-            if (this.getAttribute("service") === "illustration2") {
+            if (this.getAttribute("package") === "illustration2") {
                 bronze.style.display = "inline";
                 silver.style.display = "inline";
                 gold.style.display = "inline";
             }
-            if (this.getAttribute("service") === "fullpackage2") {
+            if (this.getAttribute("package") === "fullpackage2") {
                 bronze.style.display = "inline";
                 silver.style.display = "inline";
                 gold.style.display = "inline";
@@ -71,158 +71,190 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-function displayNext() {
-
-    document.getElementById("answer-box").value = "";
-    document.getElementById("answer-box").focus();
-
-    // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
-
-    if (gameType === "addition") {
-        displayNext(num1, num2);
-    }
-
-}
-
-function displayNext(operand1, operand2) {
-
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
-    
-}
+// Extract user's checked checkboxes 
 
 
-/**
- * The main game "loop", called when the script is first loaded
- * and after the user's answer has been processed
- */
-function runGame(gameType) {
+function brandingCheckbox(event) {
+    var logo = document.getElementById("logo");
+    var promo = document.getElementById("promo");
+    var stationary = document.getElementById("stationary");
+    var packaging = document.getElementById("packaging");
 
-    document.getElementById("answer-box").value = "";
-    document.getElementById("answer-box").focus();
-
-    // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
-
-    if (gameType === "addition") {
-        displayAdditionQuestion(num1, num2);
-    } else if (gameType === "multiply") {
-        displayMultiplyQuestion(num1, num2);
-    } else if (gameType === "subtract" ) {
-        displaySubtractQuestion(num1, num2);
-    } else if (gameType === "division"){
-	    displayDivisionQuestion(num1,num2);
+    if (document.getElementById('logo').checked){
+        alert("logo checked") ;
     } else {
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
+        alert("logo unchecked")
     }
-
-}
-
-/**
- * Checks the answer against the first element in
- * the returned calculateCorrectAnswer array
- */
-function checkAnswer() {
-
-    let userAnswer = parseInt(document.getElementById("answer-box").value);
-    let calculatedAnswer = calculateCorrectAnswer();
-    let isCorrect = userAnswer === calculatedAnswer[0];
-
-    if (isCorrect) {
-        alert("Hey! You got it right! :D");
-        incrementScore();
+    if (document.getElementById('promo').checked){
+        alert("promo checked") ;
     } else {
-        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-        incrementWrongAnswer();
+        alert("promo unchecked")
     }
-
-    runGame(calculatedAnswer[1]);
-
-}
-
-/**
- * Gets the operands (the numbers) and the operator (plus, minus etc)
- * directly from the dom, and returns the correct answer.
- */
-function calculateCorrectAnswer() {
-
-    let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById("operator").innerText;
-
-    if (operator === "+") {
-        return [operand1 + operand2, "addition"];
-    } else if (operator === "x") {
-        return [operand1 * operand2, "multiply"];
-    } else if (operator === "-") {
-        return [operand1 - operand2, "subtract"];
-    } else if (operator ==="/") {
-		return [operand1 / operand2, "division"];
+    if (document.getElementById('stationary').checked){
+        alert("stationary checked") ;
     } else {
-        alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`;
+        alert("stationary unchecked")
+    }
+    if (document.getElementById('packaging').checked){
+        alert("packaging checked") ;
+    } else {
+        alert("packaging unchecked")
     }
 
-}
+};
 
-/**
- * Gets the current score from the DOM and increments it by 1
- */
-function incrementScore() {
+// function displayNext() {
 
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
+//     document.getElementById("answer-box").value = "";
+//     document.getElementById("answer-box").focus();
 
-}
+//     // Creates two random numbers between 1 and 25
+//     let num1 = Math.floor(Math.random() * 25) + 1;
+//     let num2 = Math.floor(Math.random() * 25) + 1;
 
-/**
- * Gets the current tally of incorrect answers from the DOM and increments it by 1
- */
-function incrementWrongAnswer() {
+//     if (gameType === "addition") {
+//         displayNext(num1, num2);
+//     }
 
-    let oldScore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
+// }
+
+// function displayNext(operand1, operand2) {
+
+//     document.getElementById('operand1').textContent = operand1;
+//     document.getElementById('operand2').textContent = operand2;
+//     document.getElementById('operator').textContent = "+";
     
-}
+// }
 
-function displayAdditionQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
+// /**
+//  * The main game "loop", called when the script is first loaded
+//  * and after the user's answer has been processed
+//  */
+// function runGame(gameType) {
+
+//     document.getElementById("answer-box").value = "";
+//     document.getElementById("answer-box").focus();
+
+//     // Creates two random numbers between 1 and 25
+//     let num1 = Math.floor(Math.random() * 25) + 1;
+//     let num2 = Math.floor(Math.random() * 25) + 1;
+
+//     if (gameType === "addition") {
+//         displayAdditionQuestion(num1, num2);
+//     } else if (gameType === "multiply") {
+//         displayMultiplyQuestion(num1, num2);
+//     } else if (gameType === "subtract" ) {
+//         displaySubtractQuestion(num1, num2);
+//     } else if (gameType === "division"){
+// 	    displayDivisionQuestion(num1,num2);
+//     } else {
+//         alert(`Unknown game type: ${gameType}`);
+//         throw `Unknown game type: ${gameType}. Aborting!`;
+//     }
+
+// }
+
+// /**
+//  * Checks the answer against the first element in
+//  * the returned calculateCorrectAnswer array
+//  */
+// function checkAnswer() {
+
+//     let userAnswer = parseInt(document.getElementById("answer-box").value);
+//     let calculatedAnswer = calculateCorrectAnswer();
+//     let isCorrect = userAnswer === calculatedAnswer[0];
+
+//     if (isCorrect) {
+//         alert("Hey! You got it right! :D");
+//         incrementScore();
+//     } else {
+//         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+//         incrementWrongAnswer();
+//     }
+
+//     runGame(calculatedAnswer[1]);
+
+// }
+
+// /**
+//  * Gets the operands (the numbers) and the operator (plus, minus etc)
+//  * directly from the dom, and returns the correct answer.
+//  */
+// function calculateCorrectAnswer() {
+
+//     let operand1 = parseInt(document.getElementById('operand1').innerText);
+//     let operand2 = parseInt(document.getElementById('operand2').innerText);
+//     let operator = document.getElementById("operator").innerText;
+
+//     if (operator === "+") {
+//         return [operand1 + operand2, "addition"];
+//     } else if (operator === "x") {
+//         return [operand1 * operand2, "multiply"];
+//     } else if (operator === "-") {
+//         return [operand1 - operand2, "subtract"];
+//     } else if (operator ==="/") {
+// 		return [operand1 / operand2, "division"];
+//     } else {
+//         alert(`Unimplemented operator ${operator}`);
+//         throw `Unimplemented operator ${operator}. Aborting!`;
+//     }
+
+// }
+
+// /**
+//  * Gets the current score from the DOM and increments it by 1
+//  */
+// function incrementScore() {
+
+//     let oldScore = parseInt(document.getElementById("score").innerText);
+//     document.getElementById("score").innerText = ++oldScore;
+
+// }
+
+// /**
+//  * Gets the current tally of incorrect answers from the DOM and increments it by 1
+//  */
+// function incrementWrongAnswer() {
+
+//     let oldScore = parseInt(document.getElementById("incorrect").innerText);
+//     document.getElementById("incorrect").innerText = ++oldScore;
     
-}
+// }
 
-function displaySubtractQuestion(operand1, operand2) {
+// function displayAdditionQuestion(operand1, operand2) {
 
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
-    document.getElementById('operator').textContent = "-";
+//     document.getElementById('operand1').textContent = operand1;
+//     document.getElementById('operand2').textContent = operand2;
+//     document.getElementById('operator').textContent = "+";
+    
+// }
 
-}
+// function displaySubtractQuestion(operand1, operand2) {
 
-function displayMultiplyQuestion(operand1, operand2) {
+//     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+//     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+//     document.getElementById('operator').textContent = "-";
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "x";
+// }
 
-}
+// function displayMultiplyQuestion(operand1, operand2) {
 
-// Add your division question here
-function displayDivisionQuestion(operand1, operand2) {
-    operand1 = operand1*operand2
-	document.getElementById("operand1").textContent = operand1;
-	document.getElementById("operand2").textContent = operand2;
-	document.getElementById("operator").textContent = "/";
+//     document.getElementById('operand1').textContent = operand1;
+//     document.getElementById('operand2').textContent = operand2;
+//     document.getElementById('operator').textContent = "x";
 
-}
+// }
 
-function refresh(){
-    window.location.reload("Refresh")
-}
+// // Add your division question here
+// function displayDivisionQuestion(operand1, operand2) {
+//     operand1 = operand1*operand2
+// 	document.getElementById("operand1").textContent = operand1;
+// 	document.getElementById("operand2").textContent = operand2;
+// 	document.getElementById("operator").textContent = "/";
+
+// }
+
+// function refresh(){
+//     window.location.reload("Refresh")
+// }
